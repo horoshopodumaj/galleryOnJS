bf91fd5e1f073f697296ccdeb52bdd89439ea94c
Diana Murtazina <murka8831@yandex.ru>
2022-11-07 23:02:11 +0400

    markup via class

--- gallery-lib/gallery.js
+++ gallery-lib/gallery.js
@@ -1,3 +1,7 @@
+const GalleryClassName = "gallery";
+const GalleryLineClassName = "gallery-line";
+const GallerySlideClassName = "gallery-slide";
+
 class Gallery {
     constructor(element, options = {}) {
         this.containerNode = element;
@@ -8,5 +12,29 @@ class Gallery {
         this.manageHTML();
     }
 
-    manageHTML() {}
+    manageHTML() {
+        this.containerNode.classList.add(GalleryClassName);
+        this.containerNode.innerHTML = `
+            <div class="${GalleryLineClassName}">
+                ${this.containerNode.innerHTML}
+            </div>
+        `;
+        this.lineNode = this.containerNode.querySelector(`.${GalleryLineClassName}`);
+        this.slideNodes = Array.from(this.lineNode.children).map((childNode) =>
+            wrapElementByDiv({
+                element: childNode,
+                className: GallerySlideClassName,
+            })
+        );
+    }
+}
+
+function wrapElementByDiv({ element, className }) {
+    const wrapperNode = document.createElement("div");
+    wrapperNode.classList.add(className);
+
+    element.parentNode.insertBefore(wrapperNode, element);
+    wrapperNode.appendChild(element);
+
+    return wrapperNode;
 }
